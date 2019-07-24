38300c056dd37ba5389f75fab2c37e2cf370513d
evgentn <evgentitovn@gmail.com>
2019-07-24 20:07:59 +0300

    four

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:<evgentn>/<evgentn>.github.io.git master
+git push -f git@github.com:evgentn/evgentn.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:evgentn/tntest.git master:gh-pages
